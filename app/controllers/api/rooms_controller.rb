0b8f490ee7cb9b6b717ee143af70e3740cbd8bea
Mark Passando <markpassando@gmail.com>
2017-06-20 11:34:48 -0400

    Create Room controller

---
+++ app/controllers/api/rooms_controller.rb
@@ -0,0 +1,27 @@
+class Api::RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+    render :index
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      render :show
+    else
+      render json: @room.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:host_id, :title, :price, :num_guests,
+    :bedrooms, :beds, :description, :rules, :prop_type, :room_type,
+    :wifi, :kitchen, :ac, :tv)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
+    resources :rooms, only: [:index, :show, :create]
   end
 end
