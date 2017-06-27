72d923e913551d111181f63db56260551edfdb39
Mark Passando <markpassando@gmail.com>
2017-06-26 21:59:13 -0400

    Style Receipt Page

--- frontend/components/trip/item/TripPage.jsx
+++ frontend/components/trip/item/TripPage.jsx
@@ -7,7 +7,7 @@ class TripPage extends React.Component {
     this.props.fetchTrip(this.props.match.params.tripId);
   }
 
-  renderReceipt(){
+  calculateDays(){
     const { trip } = this.props;
     let checkIn = new Date(trip.check_in);
     let checkOut = new Date(trip.check_out);
@@ -15,41 +15,75 @@ class TripPage extends React.Component {
     let timeDiff = Math.abs(checkOut.getTime() - checkIn.getTime());
     let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-    return (<p>{diffDays} days</p>);
+    return diffDays;
+  }
+
+  renderReceipt(){
+    const { trip } = this.props;
+    const num_nights = this.calculateDays();
+    const price_nights = num_nights * trip.room.price;
+    const cleaning_fee = Math.floor(trip.room.price / 8);
+    const service_fee = Math.floor((price_nights + cleaning_fee) / 8);
+    const total = price_nights + cleaning_fee + service_fee;
+
+    return(
+      <ul className="price-table">
+        <li>
+          <div>{`$${trip.room.price} x ${num_nights} nights`}</div>
+          <div>{`$${price_nights}`}</div>
+        </li>
+        <li>
+          <div>Cleaning fee</div>
+          <div>{`$${cleaning_fee}`}</div>
+        </li>
+        <li>
+          <div>Service fee</div>
+          <div>{`$${service_fee}`}</div>
+        </li>
+        <li>
+          <div><strong>Total</strong></div>
+          <div><strong>{`$${total}`}</strong></div>
+        </li>
+      </ul>
+    );
   }
 
   render() {
     const { trip } = this.props;
     if (!trip) return null;
 
+    const imgStyle = {
+      height: "100%",
+      width: "100%",
+      backgroundImage: `url(${trip.room.room_pic_url})`
+    };
+
     return(
       <article>
-        <RoomImageContainer room={trip.room} />
         <section className="container">
-          <div className="room-page room-detail">
-            <div className="rm-detail-title rm-detail-bottom">
-              <div>
-                <h2>{trip.room.title}</h2>
-                <p>{trip.room.address}</p>
-              </div>
-              <div className="host-container">
-                <div>
-                  <img src={trip.room.avatar_url} />
+          <div className="trip-item">
+            <div className="room-index-img-box">
+              <Link to={`/rooms/${trip.room.id}`}>
+                <div className="room-index-img" style={imgStyle}></div>
+                <div className="host-container">
+                    <img src={trip.room.host_pic_url} />
                 </div>
-                <div>
-                  <p>{trip.room.username}</p>
-                </div>
+              </Link>
+            </div>
+
+            <div className="trip-info item">
+              <Link className="trip-link" to={`/rooms/${trip.room.id}`}>{trip.room.title}</Link>
+              <p>{trip.check_in} - {trip.check_out}</p>
+              <p><strong>Status: </strong>{trip.status}</p>
+              <p>{trip.num_guests} Guests</p>
+
+              <div className="receipt-box">
+                {this.renderReceipt()}
               </div>
             </div>
-          </div>
 
-          <Link to={`/rooms/${trip.room.id}`}><img src={trip.room.room_pic_url} /></Link>
-          <img src={trip.room.host_pic_url} />
-          <Link to={`/rooms/${trip.room.id}`}>{trip.room.title}</Link>
-          <p>Status: {trip.status}</p>
-          <p>{trip.num_guests} Guests</p>
-          <p>{trip.check_in} - {trip.check_out}</p>
-          {this.renderReceipt()}
+
+          </div>
         </section>
       </article>
     );
