f42a01d8ab1c547eb531ddcccb11bb541073c60d
Mark Passando <markpassando@gmail.com>
2017-07-13 14:05:15 -0400

    Fix Logger store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,8 +7,8 @@ import thunk from 'redux-thunk';
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  const { logger } = require('redux-logger');
-  middlewares.push(logger);
+  const { createLogger } = require('redux-logger');
+  middlewares.push(createLogger());
 }
 
 const configureStore = (preloadedState = {}) => (
