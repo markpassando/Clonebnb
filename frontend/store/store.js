eb4df1bb366f21dd3a5c763591601a6c4cd7e68d
Mark Passando <markpassando@gmail.com>
2017-07-13 13:58:59 -0400

    Fix package json logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 // import logger from 'redux-logger';
 
-import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
