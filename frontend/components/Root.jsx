55d8b713fd7b146e139a6884334b1a3ee95b65c9
Mark Passando <markpassando@gmail.com>
2017-07-16 17:56:48 -0400

    Configure Google Analytics

--- frontend/components/Root.jsx
+++ frontend/components/Root.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-60393807-3');
+
+const logPageView => () {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <HashRouter>
+    <HashRouter onUpdate={logPageView}>
       <App />
     </HashRouter>
   </Provider>
