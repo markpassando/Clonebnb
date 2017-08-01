5db466c1f44609f1c7480f2e494a9934eed6f1f9
Mark Passando <markpassando@gmail.com>
2017-08-01 11:24:28 -0400

    Temp fix filters

--- frontend/components/search/FilterForm.jsx
+++ frontend/components/search/FilterForm.jsx
@@ -1,38 +1,60 @@
 import React from 'react';
 
-const handleChange = (filter, updateFilter) => e => (
-  updateFilter(filter, e.currentTarget.value)
-);
+class FilterForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const FilterForm = ({ minGuests, minPrice, maxPrice, updateFilter }) => (
-  <div className="filter-form">
-    <div>
-      <label>Min Guests:</label>
-      <input
-        type="number"
-        value={minGuests}
-        placeholder="Number"
-        onChange={handleChange('minGuests', updateFilter)}
-        />
-    </div>
+  componentWillUnmount() {
+    this.props.updateFilter('minGuests', "");
+    this.props.updateFilter('minPrice', "");
+    this.props.updateFilter('maxPrice', "");
+  }
 
-  <div>
-      <label>Price Range:</label>
-      <input
-        type="number"
-        value={minPrice}
-        placeholder="$ Min"
-        onChange={handleChange('minPrice', updateFilter)}
-        />
 
-      <input
-        type="number"
-        value={maxPrice}
-        placeholder="$$ Max"
-        onChange={handleChange('maxPrice', updateFilter)}
-        />
-    </div>
-  </div>
-);
+  render() {
+    const { minGuests, minPrice, maxPrice, updateFilter } = this.props;
+
+    const handleChange = (filter, updateFilter) => e => (
+      updateFilter(filter, e.currentTarget.value)
+    );
+
+    return (
+      <div className="filter-form">
+        <div>
+          <label>Min Guests:</label>
+          <input
+            type="number"
+            value={minGuests}
+            placeholder="Number"
+            onChange={handleChange('minGuests', updateFilter)}
+            />
+        </div>
+
+      <div>
+          <label>Price Range:</label>
+          <input
+            type="number"
+            value={minPrice}
+            placeholder="$ Min"
+            onChange={handleChange('minPrice', updateFilter)}
+            />
+
+          <input
+            type="number"
+            value={maxPrice}
+            placeholder="$$ Max"
+            onChange={handleChange('maxPrice', updateFilter)}
+            />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
 
 export default FilterForm;
